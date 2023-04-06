22ef19eba7c41f2e0d7e626f1406e6d7e0238039
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-04-06 10:12:00 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,27 +1,10 @@
 SCRIPTDIR=$(cd `dirname $0` && pwd)
-
-if [ -e ~/.zshrc ]; then
-	mv ~/.zshrc ~/.zshrc~
-fi
-
 cd $SCRIPTDIR
-
-git submodule update --init --recursive
-
-HOSTNAME=$(hostname)
 
 mkdir -p ~/.ssh
 
-ln -fs $SCRIPTDIR/zsh/zshrc.sh ~/.zshrc
 stow git
-ln -fs $SCRIPTDIR/emacs/emacs ~/.emacs
+stow emacs
 stow vim
 stow fish
-ln -fs $SCRIPTDIR/sbt/sbtconfig ~/.sbtconfig
-ln -fs $SCRIPTDIR/screen/screenrc ~/.screenrc
-
-if [ -e /usr/bin/xmonad ]; then
-    mkdir -p ~/.xmonad
-    ln -fs $SCRIPTDIR/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-    ln -fs $SCRIPTDIR/xmonad/xmobarrc ~/xmobarrc
-fi
+stow screen
