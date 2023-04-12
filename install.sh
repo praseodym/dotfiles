38f6d80b158002ef6ee698285d7612c3a4a44868
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-04-12 12:56:01 +0200

    Install GNU Stow

--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@ cd $SCRIPTDIR
 
 mkdir -p ~/.ssh
 
+if ! command -v stow > /dev/null; then
+  if command -v apt-get > /dev/null; then
+    echo "installing stow"
+    apt-get update && apt-get install -y stow
+  else
+    echo "cannot find stow and can't install it"
+  fi
+fi
+
+echo "creating symlinks"
 stow git
 stow emacs
 stow vim
