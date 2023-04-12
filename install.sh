1cdd0432db9737594c72aa4dcd5630fa88d0b172
Mark Janssen <20283+praseodym@users.noreply.github.com>
2023-04-12 13:00:59 +0200

    Use sudo for apt-get

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ mkdir -p ~/.ssh
 if ! command -v stow > /dev/null; then
   if command -v apt-get > /dev/null; then
     echo "installing stow"
-    apt-get update && apt-get install -y stow
+    id
+    sudo apt-get update && sudo apt-get install -y stow
   else
     echo "cannot find stow and can't install it"
   fi
